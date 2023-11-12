fb6b440d2517be81f8b4670133123b600cf8274e
Matt Zhang <matt@zhang.software>
2023-11-11 21:32:58 -0500

    yo

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-# This example requires the 'message_content' intent.
-
 import discord
-
 import os
 from dotenv import load_dotenv
 load_dotenv()
+
+#amogus discord bot
+class Game: 
+    def __init__(self, players, tasks_to_win, imposter_count):
+        self.players = players
+        self.tasks_to_win = tasks_to_win
+        self.imposter_count = imposter_count
+    
+    def send_roles():
+        pass
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -15,12 +22,19 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
+# sign up for next game
+# start game
+#    - start timer
+# meeting
+# task counter
+# reset game
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('.getRole'):
         await message.channel.send('Hello!')
 
 client.run(os.getenv("SECRET_TOKEN"))
